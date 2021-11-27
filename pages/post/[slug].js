017769f499955e46ed65fa67c602da5499ad3c81
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-11-27 19:28:26 +0100

    build bug fixed?

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,23 +1,17 @@
-import React from "react";
-import { useRouter } from "next/router";
-import { getPosts, getPostDetails } from "../../services";
-import {
-  PostDetail,
-  Categories,
-  PostWidget,
-  Author,
-  Comments,
-  CommentsForm,
-  Loader,
-} from "../../components";
+import React from 'react';
+import { useRouter } from 'next/router';
+
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import { getPosts, getPostDetails } from '../../services';
+import { AdjacentPosts } from '../../sections';
 
 const PostDetails = ({ post }) => {
-  // console.log(post);
-  const router = useRoute();
+  const router = useRouter();
 
   if (router.isFallback) {
     return <Loader />;
   }
+
   return (
     <>
       <div className="container mx-auto px-10 mb-8">
@@ -25,16 +19,13 @@ const PostDetails = ({ post }) => {
           <div className="col-span-1 lg:col-span-8">
             <PostDetail post={post} />
             <Author author={post.author} />
-            {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
             <CommentsForm slug={post.slug} />
             <Comments slug={post.slug} />
           </div>
           <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">
-              <PostWidget
-                slug={post.slug}
-                categories={post.categories.map((category) => category.slug)}
-              />
+              <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
               <Categories />
             </div>
           </div>
@@ -43,16 +34,18 @@ const PostDetails = ({ post }) => {
     </>
   );
 };
-
 export default PostDetails;
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
-    props: { post: data },
+    props: {
+      post: data,
+    },
   };
 }
+
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
