79281f4aa1ce3367575eb55313f0b4d63615794f
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-11-27 21:13:51 +0100

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      spacing: {
-        '2/3': '66.666667%',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-
-};
+}
