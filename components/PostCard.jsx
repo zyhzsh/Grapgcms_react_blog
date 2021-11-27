130818feffaac3c1713b5922a76b3c2aa6b541fa
Zhu,Shenghang S <s.zhu@student.fontys.nl>
2021-11-28 00:47:51 +0100

    change color

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -21,7 +21,7 @@ const PostCard = ({ post }) => (
       <img src={post.featuredImage.url} alt="" className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
     </div>
 
-    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-blue-600 text-3xl font-semibold">
       <Link href={`/post/${post.slug}`}>{post.title}</Link>
     </h1>
     <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
